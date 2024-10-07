96b1d362a76a3ed01d70f0362a45bd6d8f3e9b6c
Martín Lombardo <to@mlomb.me>
2024-10-06 22:32:57 -0300

    add cpp pragmas

--- bundler/src/cpp/mod.rs
+++ bundler/src/cpp/mod.rs
@@ -1,32 +1,29 @@
 mod expander;
 
-use expander::CppExpander;
-
 use crate::bundler::Bundler;
-use std::{
-    error::Error,
-    fs,
-    path::{Path, PathBuf},
-};
+use expander::CppExpander;
+use std::{error::Error, path::Path};
 
 pub struct CppBundler {}
-
-impl CppBundler {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
 
 impl Bundler for CppBundler {
     /// Checks if a file is a C/C++ entrypoint
     fn is_entrypoint(path: &Path) -> bool {
-        let ext = path.extension().unwrap_or_default().to_ascii_lowercase();
-        ext == "cpp" || ext == "c"
+        let name = path.file_name().unwrap_or_default().to_ascii_lowercase();
+        name == "main.cpp" || name == "main.c"
     }
 
     fn bundle(main_path: &Path) -> Result<String, Box<dyn Error>> {
         assert!(Self::is_entrypoint(main_path));
 
-        CppExpander::new().expand_source(main_path)
+        let source = CppExpander::new().expand_source(main_path)?;
+
+        Ok(format!("{}\n{}", PRAGMAS, source))
     }
 }
+
+static PRAGMAS: &str = r###"#pragma warning( disable : 4068 ) // unknown pragma
+#pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline")
+#pragma GCC option("arch=native","tune=native","no-zero-upper")
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2")
+"###;
