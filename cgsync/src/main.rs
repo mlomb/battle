384361384d5517616986b3d2cba02d677fdd606b
Martín Lombardo <to@mlomb.me>
2024-11-07 19:15:05 -0300

    minor terminal color additions

--- cgsync/src/main.rs
+++ cgsync/src/main.rs
@@ -1,17 +1,18 @@
 mod server;
 
-use bundler::bundle;
+use bundler::{bundle, BundlerArgs};
 use clap::Parser;
+use console::style;
 use futures_channel::mpsc::channel;
 use notify::{RecursiveMode, Watcher};
 use server::start_server;
-use std::path::PathBuf;
+use std::collections::HashSet;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(default_value = ".")]
-    project_path: PathBuf,
+    #[clap(flatten)]
+    bundler_args: BundlerArgs,
 }
 
 #[tokio::main]
@@ -22,27 +23,34 @@ async fn main() {
 
     start_server(code_rx).await;
 
-    let watch_path = args.project_path.clone();
     let (evt_tx, evt_rx) = std::sync::mpsc::channel();
     let mut watcher = notify::recommended_watcher(evt_tx).unwrap();
-    watcher
-        .watch(&watch_path, RecursiveMode::Recursive)
-        .unwrap();
-    watcher
-        .watch(&watch_path.join("src"), RecursiveMode::Recursive)
-        .unwrap();
-    watcher.unwatch(&watch_path.join("target")).unwrap();
-
-    println!("Watching \"{}\"", watch_path.display());
+    let mut currently_watching = HashSet::new();
 
     loop {
         // bundle and send the code
-        match bundle(&args.project_path) {
+        match bundle(&args.bundler_args) {
             Ok(bundle) => {
                 code_tx.try_send(bundle.source).unwrap();
+                println!("{} Code bundled and updated", style("U").green());
+
+                for file in bundle.src_files {
+                    if currently_watching.contains(&file) {
+                        continue;
+                    }
+
+                    println!(
+                        "{} Added file to watch: {}",
+                        style("W").yellow(),
+                        file.file_name().unwrap().to_str().unwrap()
+                    );
+
+                    watcher.watch(&file, RecursiveMode::NonRecursive).unwrap();
+                    currently_watching.insert(file);
+                }
             }
             Err(e) => {
-                println!("Failed to bundle: {:?}", e);
+                println!("{} Failed to bundle: {:?}", style("E").red(), e);
             }
         }
 
@@ -50,7 +58,8 @@ async fn main() {
         let evt = evt_rx.recv().unwrap();
 
         println!(
-            "Changes detected: {}",
+            "{} Changes detected: {}",
+            style("C").blue(),
             evt.unwrap()
                 .paths
                 .iter()
